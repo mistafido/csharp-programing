2f47a111de662a3c3e17ff20a7c39e85ab227842
unknown <Frederick.o@vihanafashions.co.in>
2022-06-09 21:20:32 +0530

    added new files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Employee
+    {
+        private string Name;
+        private int Age;
+        public int GetAge()
+        {
+            return Age;
+        }
+        public void SetAge(int age)
+        {
+            if(age>60)
+            {
+                Console.WriteLine("Employee age cannot be more than 60");
+            }
+            else
+            {
+                this.Age = age;
+            }
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        public void SetName(string name)
+        {
+            if(name.IndexOf('?') > 0)
+            {
+                Console.WriteLine("Illegal Character cannot be in the name");
+            }
+            else
+            {
+                this.Name = name;
+            }
+        }
+    }
+    
+    public class MethodEmployee
+    {
+        private string name;
+        private int age;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                { name = value; }
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                { age = value; }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee first = new Employee();
+            MethodEmployee second = new MethodEmployee();
+            second.Name = "James Bond";
+            second.Age = 50;
+            first.SetAge(50);
+            first.SetName("Amir Khan");
+            Console.WriteLine("Employee Name:" + first.GetName());
+            Console.WriteLine("Employee Age:" + first.GetAge());
+            Console.WriteLine("Employee Name:" + second.Name);
+            Console.WriteLine("Employee Age:" + second.Age);
+            Console.ReadKey();
+
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+    }
+}
---
+++ Sample project/Sample project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sample_project.MvcApplication" Language="C#" %>
